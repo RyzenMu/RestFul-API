e5038d84e1e03efbac5aed4d017ea596e3b9d314
RyzenMu <muruga13in2022@gmail.com>
2024-09-30 04:50:01 +0530

    completed

--- app.js
+++ app.js
@@ -23,36 +23,67 @@ mongoose
   )
   .then(() => {
     console.log("Mongo Connected");
-    //////////GET DOCUMENTS ////////////
-app.get("/articles", async function (req, res) {
-  try {
-    const foundArticles = await Article.find().lean(); // Use await without callbacks
-    if (foundArticles.length === 0) {
-      console.log("No articles found");
-    } else {
-      console.log(foundArticles);
-    }
-    res.send(foundArticles);
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-app.post("/articles", function (req, res) {
-  console.log(req.body.title);
-  console.log(req.body.content);
-  const newArticle = new Article({
-    title : req.body.title,
-    content: req.body.content
-  });
-  newArticle.save();    
-  });
+    //////////DOCUMENTS ROUTE ////////////
+    app
+      .route("/articles")
+      .get(async function (req, res) {
+        try {
+          const foundArticles = await Article.find().lean(); // Use await without callbacks
+          if (foundArticles.length === 0) {
+            console.log("No articles found");
+          } else {
+            console.log(foundArticles);
+          }
+          res.send(foundArticles);
+        } catch (error) {
+          console.log(error);
+        }
+      })
+      .post(function (req, res) {
+        console.log(req.body.title);
+        console.log(req.body.content);
+        const newArticle = new Article({
+          title: req.body.title,
+          content: req.body.content,
+        });
+        newArticle.save();
+      })
+      .delete(async function (req, res) {
+        await Article.deleteMany();
+        res.send("Deleted all articles");
+      });
+    /////////SINGLE ARTICLE//////////
+    app
+      .route("/articles/:title")
+      .get(async function (req, res) {
+        const titleF = req.params.title;
+        const foundDoc = await Article.findOne({ title: titleF });
+        if (!foundDoc) {
+          console.log("No Matching records found");
+        } else {
+          console.log(foundDoc);
+          res.send(foundDoc);
+        }
+      })
+      .put(async function (req, res) {
+        await Article.findOneAndUpdate({title:req.params.title}, {title:req.body.title,
+          content: req.body.content
+        });
+        console.log('Updated Successfully');        
+      })
+      .patch(async function (req, res) {
+        await Article.findOneAndUpdate({title:req.params.title}, {
+          content: req.body.content
+        });
+        console.log('Patched Successfully');        
+      })
+      .delete(async function (req, res) {
+        await Article.findOneAndDelete({title:req.params.title});
+        console.log('Deleted Successfully');        
+      })
+      
   })
   .catch((err) => console.error(err));
-
-
-
-
 
 app.listen(3000, () => {
   console.log("Server Started");
